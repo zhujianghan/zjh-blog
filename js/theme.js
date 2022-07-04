96bf8fe0f1db06a3a07e8cc7b829a2cf47d4d9e8
zhujianghan <zhujianghan@users.noreply.github.com>
2022-07-04 02:37:39 +0000

    deploy: e420428a322b639caba9b710853f1ef3395b7ca7

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,13 @@
-if (localStorage.theme === 'dark') {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
+window.setTheme = (t = '') => {
+  if (t === 'dark' || t === 'light') {
+    localStorage.theme = t
+  }
+
+  if (localStorage.theme === 'dark') {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
 }
+
+window.setTheme()
